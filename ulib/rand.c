90279e6f399974cfe8c0d7cff04dbc0531bd0738
Momchil Velikov <velco@fadata.bg>
2006-01-12 21:29:25 +0000

    add a random shuffle function

---
+++ ulib/rand.c
@@ -0,0 +1,26 @@
+#include "rand.h"
+
+/* Generate a random permutation of the elements of the vector V of
+   length N.  */
+void
+ulib_shuffle (unsigned int n, unsigned int *v)
+{
+  unsigned int i, r, tmp;
+
+  for (i = 0; i < n; ++i)
+    {
+      r = ulib_rand (0, n - 1);
+      tmp = v [r];
+      v [r] = v [i];
+      v [i] = tmp;
+    }
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * indent-tabs-mode: nil
+ * arch-tag: c0545edf-696a-49a4-a9dc-6fbb6d97c5f1
+ * End:
+ */
+
--- ulib/rand.h
+++ ulib/rand.h
@@ -2,6 +2,7 @@
 #define ulib__rand_h 1
 
 #include "defs.h"
+#include "ulib-if.h"
 #include <stdlib.h>
 #include <limits.h>
 
@@ -13,6 +14,10 @@ ulib_rand (unsigned int lo, unsigned int hi)
 {
   return (unsigned int) (rand () / (RAND_MAX + 1.0) * (hi - lo + 1) + lo);
 }
+
+/* Generate a random permutation of the elements of the vector V of
+   length N.  */
+ULIB_IF void ulib_shuffle (unsigned int n, unsigned int *v);
 
 END_DECLS
 #endif /* ulib__rand_h */
